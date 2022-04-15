5d2fcc56cd72eac9591b5aa3aef1a45db16783cb
gorkemer <gorkemerr@gmail.com>
2022-04-14 20:16:07 -0600

    cloning fgm analysis

--- bgm_plotting.R
+++ bgm_plotting.R
@@ -32,106 +32,151 @@ bgmdata.r = subset(bgmdata, bgmdata$randomTrialsR1C0 == 1)
 bgmndata = read.csv("bgmndata.csv",header=TRUE, quote="\"") 
 bgmndata.c = subset(bgmndata, bgmndata$randomTrialsR1C0 == 0)
 bgmndata.r = subset(bgmndata, bgmndata$randomTrialsR1C0 == 1)
+# April 12th, 2022.
+# Plotting script for the foreground motion experiment. 
+# Same/similar to bgm_plotting.R script. Only doing it for the fgm plots
+# plots to be used for the manuscript
+# good representative participant: 939045
 
-#plotREF_GO(bgmdata, bgmdata$response_error, bgmdata$uncuedAR, bgmdata$sameDirection1S0D)
+# clear the workspace
+rm(list=ls()) 
 
-interactionPlot <- ggplot(bgmndata.c, aes(x = uncuedAR, y = normedR_indv, colour=as.factor(sameDirection1S0D))) + 
-  #geom_point(shape=11, size=0.5, alpha=0.01, show.legend = FALSE) +
-  geom_jitter(shape=11, size=0.5, alpha=0.01, show.legend = FALSE) +
-  #geom_density_2d(color="gray", alpha=0.3) + 
-  coord_cartesian(ylim=c(-0.05, 0.05)) +
-  theme(legend.key.size = unit(0.2, "cm")) + 
-  geom_smooth(method = "lm", span = 0.1, alpha= 0.2,
+# load libraries in bulk
+x<-c("ggpubr", "ggplot2", "multcomp", "pastecs", "tidyr","dplyr", "ggiraph", "ggiraphExtra", "plyr", 
+     "covreg", "plot3D", "Hmisc", "corrplot", "psych", "tidyverse", "hrbrthemes", "viridis", "gapminder",
+     "ggExtra", "scatterplot3d", "reshape2", "rlang", "plyr", "data.table", "lme4", "magrittr", "fitdistrplus",
+     "gridExtra", "statmod", "dotwhisker")
+
+require(x)
+lapply(x, require, character.only = TRUE)
+
+source("~/Documents/GitHub/ger_R_functions/plot_functions.R")
+
+savePlot <- function(myPlot) {
+  pdf("myPlot.pdf")
+  print(myPlot)
+  dev.off()
+}
+
+#### locate the data and import it ####
+# loading data
+setwd('~/Desktop/21Projects/Single_FG_Motion')
+fgmdata = read.csv('fgmdata.csv', header = TRUE)
+
+#### identify/rename variables for plotting ####
+
+# plot by cued vector vs uncued vector
+fgmdata$cued_vector <- ifelse(fgmdata$cued_ellipse == 1, fgmdata$e1_motion_dir, fgmdata$e2_motion_dir)
+fgmdata$uncued_vector <- ifelse(fgmdata$cued_ellipse == 1, fgmdata$e2_motion_dir, fgmdata$e1_motion_dir)
+
+# reorder levels
+fgmdata$cued_vector <- factor(fgmdata$cued_vector, levels = c(0, 180, 90, 270))
+fgmdata$uncued_vector <- factor(fgmdata$uncued_vector, levels = c(0, 180, 90, 270))
+
+# rename levels
+fgmdata$cued_vector <- factor(fgmdata$cued_vector, labels = c( sprintf('\u2192'), sprintf('\u2190'), sprintf('\u2191'), sprintf('\u2193')))
+fgmdata$uncued_vector <- factor(fgmdata$uncued_vector, labels = c(sprintf('\u2192'), sprintf('\u2190'), sprintf('\u2191'),sprintf('\u2193')))
+
+fgmdata$global_org <- factor(fgmdata$global_org, labels = c("between", "within" ))
+
+#### plotting variables ####
+yaxisLim <- 0.1
+densityAlpha <- 0.1
+densityColor <- "blue"
+jitterAlpha <- 0.1
+identicalMotion_label <- "Same"
+differentMotion_label <- "Different"
+identicalMotion_color <- "red"
+differentMotion_color <- "black"
+lmAlpha <- 0.3
+
+# remove scientific notation in the entire R session
+options(scipen = 100)
+
+# for plotting on the aggregated data 
+fgmdata.a <- aggregate(responseError ~ uncuedAR  + sameDirection1S0D + sub, fgmdata, mean)
+
+#### fgm plot 01 ####
+
+bgmplot03 <- ggplot(fgmdata, aes(x = uncuedAR, y = responseError, colour=as.factor(sameDirection1S0D))) + 
+  #geom_point(shape=1, size=0.5, alpha=jitterAlpha, show.legend = FALSE) +
+  #geom_jitter(shape=1, size=0.5, alpha=jitterAlpha, show.legend = FALSE) +
+  #geom_density_2d(color=densityColor, alpha=densityAlpha) + 
+  coord_cartesian(ylim=c(-yaxisLim, yaxisLim)) +
+  geom_smooth(method = "lm", span = 1, alpha= lmAlpha,
               aes(color = as.factor(sameDirection1S0D))) +
   geom_segment(aes(x=min(unique(uncuedAR)),xend=max(unique(uncuedAR)),y=0,yend=0), linetype="longdash",  color="gray50")+ 
-  # scale_color_manual(name="Shared Box"
-  #                    #labels=c("cued (1/2)","Neither inside", "Shared")
-  #                    ,values=c("black",
-  #                              "red","orange", "blue", "slateblue3" ))+
   scale_color_manual(name="Motion Direction",
-                     labels=c("Unshared","Shared")
-                     ,values=c("red",
-                               "black"))+
-  labs(x="(<-flatter)   Uncued AR   (taller->)", y = "(<-flatter) AR Response Error (taller->)") +
+                     labels=c(differentMotion_label,identicalMotion_label)
+                     ,values=c(differentMotion_color,
+                               identicalMotion_color))+
+  labs(x="(<-flatter)   Uncued AR   (taller->)", y = "(<-flatter) Response Error (taller->)") +
   labs(title="", subtitle=" ")+
   scale_x_continuous(breaks = seq(-0.5, 0.5, by = 0.5))+
-  theme_classic() #+
-#ggtitle("Response Error Fluctuations")
-#facet_grid(~data$cuedAR)
-interactionPlot
+  theme_classic()
+bgmplot03
+ggsave(filename = "bgmplot03.pdf", width = 14, height = 8, units = "in", device='pdf', dpi=700) 
 
-basicPlot <- ggplot(bgmndata.c, aes(x = cuedAR, y = responseAR)) + 
-  geom_jitter(shape=11, size=0.5, alpha=0.02, show.legend = FALSE) +
-  #geom_density_2d(color="gray", alpha=0.3) + 
-  #coord_cartesian(ylim=c(-0.05, 0.05)) +
-  theme(legend.key.size = unit(0.2, "cm")) + 
-  geom_smooth(method = "lm", span = 0.1, alpha= 0.2) +
-  geom_segment(aes(x=min(unique(cuedAR)),xend=max(unique(cuedAR)),y=0,yend=0), linetype="longdash",  color="gray50")+ 
-  # scale_color_manual(name="Shared Box"
-  #                    #labels=c("cued (1/2)","Neither inside", "Shared")
-  #                    ,values=c("black",
-  #                              "red","orange", "blue", "slateblue3" ))+
-  # scale_color_manual(name="Motion Direction",
-  #                    labels=c("Unshared","Shared")
-  #                    ,values=c("red",
-  #                              "black"))+
-  labs(x="(<-flatter)   Cued AR   (taller->)", y = "(<-flatter) AR Response Error (taller->)") +
-  labs(title="", subtitle=" ")+
-  scale_x_continuous(breaks = seq(-0.5, 0.5, by = 0.5))+
-  theme_classic() #+
-#ggtitle("Response Error Fluctuations")
-#facet_grid(~data$cuedAR)
-basicPlot
+#### fgm plot 02 ####
 
-interactionPlot_id <- ggplot(bgmndata.c, aes(x = uncuedAR, y = normedR_indv, colour=as.factor(sameDirection1S0D))) + 
-  #geom_point(shape=11, size=0.5, alpha=0.01, show.legend = FALSE) +
-  geom_jitter(shape=11, size=0.5, alpha=0.01, show.legend = FALSE) +
-  geom_density_2d(color="gray", alpha=0.3) + 
-  coord_cartesian(ylim=c(-0.15, 0.15)) +
-  theme(legend.key.size = unit(0.2, "cm")) + 
-  geom_smooth(method = "lm", span = 0.1, alpha= 0.2,
+fgmdata.a <- aggregate(responseError ~ uncuedAR  + sameDirection1S0D + sub + global_org+
+                         cued_vector + uncued_vector, fgmdata, mean)
+jitterAlpha <- 0.05
+fgmplot02 <- ggplot(fgmdata.a, aes(x = uncuedAR, y = responseError, colour=as.factor(sameDirection1S0D))) + 
+  #geom_point(shape=11, size=0.5, alpha=0.5, show.legend = FALSE) +
+  geom_jitter(shape=1, size=0.5, alpha=jitterAlpha, show.legend = FALSE) +
+  geom_density_2d(color="blue", alpha=densityAlpha) + 
+  coord_cartesian(ylim=c(-0.2, 0.2)) +
+  #theme(legend.key.size = unit(0.2, "cm")) + 
+  geom_smooth(method = "lm", span = 1, alpha= lmAlpha,
               aes(color = as.factor(sameDirection1S0D))) +
   geom_segment(aes(x=min(unique(uncuedAR)),xend=max(unique(uncuedAR)),y=0,yend=0), linetype="longdash",  color="gray50")+ 
-  # scale_color_manual(name="Shared Box"
-  #                    #labels=c("cued (1/2)","Neither inside", "Shared")
-  #                    ,values=c("black",
-  #                              "red","orange", "blue", "slateblue3" ))+
   scale_color_manual(name="Motion Direction",
-                     labels=c("Unshared","Shared")
-                     ,values=c("red",
-                               "black"))+
-  labs(x="(<-flatter)   Uncued AR   (taller->)", y = "(<-flatter) AR Response Error (taller->)") +
+                     labels=c(differentMotion_label,identicalMotion_label)
+                     ,values=c(differentMotion_color,
+                               identicalMotion_color))+
+  labs(x="(<-flatter)   Uncued AR   (taller->)", y = "(<-flatter) Response Error (taller->)") +
   labs(title="", subtitle=" ")+
   scale_x_continuous(breaks = seq(-0.5, 0.5, by = 0.5))+
-  theme_classic() #+
-#ggtitle("Response Error Fluctuations")
-#facet_grid(~data$cuedAR)
-interactionPlot_id + facet_wrap(~sub, nrow = 8)
+  theme_classic()+ 
+  facet_wrap(~global_org*cued_vector*uncued_vector, nrow = 2)+
+  theme_classic() +
+  theme(panel.spacing.x = unit(1, "lines"))
+
+bgmplot02
+jitterAlpha <- 0.1
+ggsave(filename = "bgmplot02.pdf", width = 14, height = 8, units = "in", device='pdf', dpi=700) 
 
 
-agg <- aggregate(responseAR ~ cuedAR + uncuedAR + sameDirection1S0D, bgmndata.c, mean)
+#### save plots ####
 
-agg1 <- subset(agg, agg$cuedAR==-0.46332)
-agg2 <- subset(agg, agg$cuedAR==-0.41699)
-agg3 <- subset(agg, agg$cuedAR==-0.37066)
-agg4 <- subset(agg, agg$cuedAR==-0.32432)
-agg5 <- subset(agg, agg$cuedAR==-0.27799)
-agg6 <- subset(agg, agg$cuedAR==-0.23166)
-agg7 <- subset(agg, agg$cuedAR==-0.18533)
-agg8 <- subset(agg, agg$cuedAR==-0.13900)
-agg9 <- subset(agg, agg$cuedAR==-0.09266)
-agg10 <- subset(agg, agg$cuedAR==-0.04633)
-agg11 <- subset(agg, agg$cuedAR==-0.00000)
-agg12 <- subset(agg, agg$cuedAR==0.04633)
-agg13 <- subset(agg, agg$cuedAR==0.09266)
-agg14 <- subset(agg, agg$cuedAR==0.13899)
-agg15 <- subset(agg, agg$cuedAR==0.18533)
-agg16 <- subset(agg, agg$cuedAR==0.23166)
-agg17 <- subset(agg, agg$cuedAR==0.27799)
-agg18 <- subset(agg, agg$cuedAR==0.32432)
-agg19 <- subset(agg, agg$cuedAR==0.37065)
-agg20 <- subset(agg, agg$cuedAR==0.41699)
-agg21 <- subset(agg, agg$cuedAR==0.46332)
+#ggsave(filename = "fgm_plot2.pdf", width = 14, height = 8, units = "in", device='pdf', dpi=700) 
+#savePlot(interactionPlotVectors)
+
+#### fgm plot 03 ####
+agg <- aggregate(responseAR ~ cuedAR + uncuedAR + sameDirection1S0D, fgmdata, mean)
+
+agg1 <- subset(agg, agg$cuedAR==-0.46331866)
+agg2 <- subset(agg, agg$cuedAR==-0.41698684)
+agg3 <- subset(agg, agg$cuedAR==-0.37065503)
+agg4 <- subset(agg, agg$cuedAR==-0.32432321)
+agg5 <- subset(agg, agg$cuedAR==-0.27799139)
+agg6 <- subset(agg, agg$cuedAR==-0.23165957)
+agg7 <- subset(agg, agg$cuedAR==-0.18532775)
+agg8 <- subset(agg, agg$cuedAR==-0.13899593)
+agg9 <- subset(agg, agg$cuedAR==-0.09266412)
+agg10 <- subset(agg, agg$cuedAR==-0.04633230)
+agg11 <- subset(agg, agg$cuedAR==-0.00000048)
+agg12 <- subset(agg, agg$cuedAR==0.04633134)
+agg13 <- subset(agg, agg$cuedAR==0.09266316)
+agg14 <- subset(agg, agg$cuedAR==0.13899498)
+agg15 <- subset(agg, agg$cuedAR==0.18532679)
+agg16 <- subset(agg, agg$cuedAR==0.23165861)
+agg17 <- subset(agg, agg$cuedAR==0.27799043)
+agg18 <- subset(agg, agg$cuedAR==0.32432225)
+agg19 <- subset(agg, agg$cuedAR==0.37065407)
+agg20 <- subset(agg, agg$cuedAR==0.41698588)
+agg21 <- subset(agg, agg$cuedAR==0.46331770)
 
 #add barkod to each agg lists, e.g. agg1 = agg = 1
 agg1$barkod <- 1
@@ -158,16 +203,22 @@ agg21$barkod <- 21
 
 aggToget <- rbind(agg1,agg2, agg3, agg4, agg5, agg6, agg7, agg8, agg9, agg10, agg11, agg12, agg13, agg14, agg15, agg16, agg17, agg18, agg19, agg20, agg21)
 
-barkodPlot <- ggplot(bgmndata.c, aes(x = uncuedAR, y =normedR_indv )) + 
-  #geom_point(shape=19, size=0.2, alpha=0.05, show.legend = FALSE) +
-  coord_cartesian(ylim=c(-0.55, 0.55)) +
-  geom_jitter(shape=19, size=0.2, alpha=0.01, show.legend = FALSE) +
-  geom_density_2d(color="gray", alpha=0.3) + 
-  geom_smooth(method = "lm", 
-              aes(color = factor(cuedAR)), alpha = 0.1) +
-  #xlim(-.5, .5) +
-  #ylim(-.7,.7) +
-  # labs(x="arDiff", y = "Reported aspect ratio") +
-  ggtitle("Grid by Motion Condition", subtitle = "positive slope for same, negative-ish for diff") 
-barkodPlot + scale_colour_grey() + theme_bw() + facet_grid(~sameDirection1S0D)
+aggToget$sameDirection1S0D <- factor(aggToget$sameDirection1S0D, labels = c("Ungrouped", "Grouped"))
+
+bgmplot03 <- ggplot(aggToget, aes(x = uncuedAR, y =responseAR )) + 
+  geom_point(shape=1, size=0.2, alpha=0.5, show.legend = FALSE, aes(color = as.factor(cuedAR))) +
+  coord_cartesian(ylim=c(-0.4, 0.4)) +
+  #geom_jitter(shape=1, size=0.5, alpha=jitterAlpha + 0.03, show.legend = FALSE) +
+  #geom_line(data = aggToget, aes(group = barkod, colour = as.factor(barkod))) +
+  geom_density_2d(color=densityColor, alpha=densityAlpha) + 
+  geom_smooth(method = "lm",
+              aes(color = factor(cuedAR)), alpha = lmAlpha-0.3)+
+  labs(x="(<-flatter)   Uncued AR   (taller->)", y = "(<-flatter) Response (taller->)")+ 
+  facet_grid(~sameDirection1S0D) + 
+  scale_colour_grey() + 
+  theme_classic() + 
+  theme(panel.spacing.x = unit(1.5, "lines"))
+bgmplot03
+ggsave(filename = "bgmplot03.pdf", width = 14, height = 8, units = "in", device='pdf', dpi=700) 
+
 
